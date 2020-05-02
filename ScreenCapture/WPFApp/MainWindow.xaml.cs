81d615afbe2fcb0246cf514f363d313c41435571
Pablo Bonilla <pbonilla29@hotmail.com>
2020-05-01 23:16:37 -0600

    Add initial screenshot of screen

--- ScreenCapture/WPFApp/MainWindow.xaml.cs
+++ ScreenCapture/WPFApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -23,6 +25,26 @@ namespace WPFApp
         public MainWindow()
         {
             InitializeComponent();
+            TakeScreenshot();
+        }
+
+        private void MainWindow_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                mainWindow.Close();
+            }
+        }
+
+        public void TakeScreenshot()
+        {
+            Bitmap bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics graphics = Graphics.FromImage(bitmap);
+
+            graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+
+            BitmapSource bitmapSource = BitmapConversion.ToWpfBitmap(bitmap);
+            mainImage.Source = bitmapSource;
         }
     }
 }
